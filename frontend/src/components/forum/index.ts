376fbb7b1f2776a44234eca904647834c4334827
augustdua <augustduamath@gmail.com>
2025-12-05 17:07:27 +0100

    fix: remove ForumCommentList export (file deleted)

--- frontend/src/components/forum/index.ts
+++ frontend/src/components/forum/index.ts
@@ -2,6 +2,5 @@ export { ForumTabContent } from './ForumTabContent';
 export { ForumPostCard } from './ForumPostCard';
 export { ForumReactionBar } from './ForumReactionBar';
 export { ForumQuickReplyBar } from './ForumQuickReplyBar';
-export { ForumCommentList } from './ForumCommentList';
 export { CreateForumPostModal } from './CreateForumPostModal';
 
